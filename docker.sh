636d1ea707b2649e4c27526f724ef81e1c86e8ba
Sergio Talens-Oliag <sto@mixinet.net>
2018-11-27 00:21:42 +0100

    Add init-db option to docker command

--- docker.sh
+++ docker.sh
@@ -2,15 +2,29 @@
 NAME="flaskr"
 DEV_ENV="-e FLASK_APP=flaskr -e FLASK_DEBUG=1 -e FLASK_ENV=development"
 DEV_CMD="flask run --host=0.0.0.0 --port=80"
+INIT_DB_ENV="-e FLASK_APP=flaskr"
+INIT_DB_CMD="flask init-db"
 RUN_ENV="-e STATIC_PATH=/app/flaskr/static"
 PORTS="127.0.0.1:80:80"
 VMAP="$(pwd)/app:/app"
 IMAGE="stodh/uwsgi-nginx-flaskr-security"
 
+# Compute work dir
+CALLER_SCRIPT="$(readlink -f "$0")" || true
+CALLER_SCRIPT_DIR="$(dirname "$CALLER_SCRIPT")" || true
+if [ -n "$CALLER_SCRIPT_DIR" ]; then
+	cd $CALLER_SCRIPT_DIR
+fi
+
+# Process options
 case "$1" in
 dev)
     docker run --rm -d $DEV_ENV --name "$NAME" -p "$PORTS" -v "$VMAP" \
            "$IMAGE" $DEV_CMD
+;;
+init-db)
+    docker run --rm -d $INIT_DB_ENV --name "$NAME" -p "$PORTS" -v "$VMAP" \
+           "$IMAGE" $INIT_DB_CMD
 ;;
 logs)
     docker logs -f "$NAME"
@@ -31,5 +45,5 @@ stop)
     docker stop "$NAME"
 ;;
 *)
-	echo "Uso: $0 [dev|logs|reload|run|shell|status|stop]"
+	echo "Uso: $0 [dev|logs|init-db|reload|run|shell|status|stop]"
 esac
